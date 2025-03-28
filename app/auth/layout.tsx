09d1801d93f4d458c04beb02559529b3233f3383
hameedhameedhh <111865652+ashiruhammed@users.noreply.github.com>
2025-03-28 21:49:06 +0100

    enhance AuthLayout with flexbox for improved layout structure and styling

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <div className='flex flex-col min-h-screen'>
       <div className='md:py-7 md:px-10 px-5 py-5 bg-primary'>
         <LogoIcon />
       </div>
-      {children}
+      <div className='bg-[#F6F3FB] flex flex-1 items-center justify-center'>
+        {children}
+      </div>
     </div>
   );
 }
