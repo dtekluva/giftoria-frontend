0568bf7fad6179090bde1d1109ae558967c1451a
hameedhameedhh <111865652+ashiruhammed@users.noreply.github.com>
2025-03-29 11:58:30 +0100

    add Search functionality to NavBar; implement SearchIcon component and enhance layout for better user interaction

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import LogoIcon from '../icon/logo';
+import { Input } from '../ui/input';
+import { SearchIcon } from 'lucide-react';
 
 function NavBar() {
   return (
     <div className='px-[30px] py-6 md:px-[50px] flex flex-row items-center justify-between absolute w-full z-50'>
       <LogoIcon width={80} height={30} />
-      <div>
+      <div className='flex items-center md:gap-[111px]'>
         <ul className='text-sm flex items-center gap-3 md:gap-6 text-white font-medium'>
           <li>Gift Cards</li>
           <li>Card Balance</li>
         </ul>
+        <div className='hidden md:flex items-stretch bg-white rounded-[30px] overflow-hidden'>
+          <Input
+            className='max-h-11 border-none w-full focus:ring-0 focus:border-transparent focus-visible:ring-0 focus-visible:ring-offset-0'
+            placeholder='Search gift card.....'
+          />
+          <div className='bg-[#990099] rounded-[30px] py-[10px] px-[17px]'>
+            <SearchIcon className='text-white' />
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ components/icon/search-icon.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+const SearchIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} viewBox='0 0 24 24' fill='none' {...props}>
+    <path
+      d='M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z'
+      stroke='white'
+      strokeWidth={1.5}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M22 22L20 20'
+      stroke='white'
+      strokeWidth={1.5}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
+export default SearchIcon;
