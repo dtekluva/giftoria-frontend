9f91aa775f87ebfc148eac0c7ae97b54dc01a917
ashiruhammed <hammedashiru89@gmail.com>
2025-04-15 03:24:01 +0100

    feat: update Business Profile layout with new tab structure for Company Details and Branch

--- app/(admin)/admin/(business-profile)/layout.tsx
+++ app/(admin)/admin/(business-profile)/layout.tsx
@@ -1,19 +1,19 @@
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import React from 'react';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 function BusinessProfileLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className='container mx-auto'>
-      <div className='mt-7'>
-        <Tabs defaultValue='account' className='px-7'>
-          <TabsList>
-            <TabsTrigger value='account'>Account</TabsTrigger>
-            <TabsTrigger value='password'>Password</TabsTrigger>
+      <div className='mt-7 w-full'>
+        <Tabs defaultValue='details' className='px-7 w-full border-b-2'>
+          <TabsList className='w-full md:max-w-[600px]'>
+            <TabsTrigger className='md:pb-4' value='details'>
+              Company Details
+            </TabsTrigger>
+            <TabsTrigger className='md:pb-4' value='branch'>
+              Branch
+            </TabsTrigger>
           </TabsList>
-          <TabsContent value='account'>
-            Make changes to your account here.
-          </TabsContent>
-          <TabsContent value='password'>Change your password here.</TabsContent>
         </Tabs>
       </div>
       {children}
