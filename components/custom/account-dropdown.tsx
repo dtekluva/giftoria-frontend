5910bba2dcd48083deac340f6c941d308ac966e3
ashiruhammed <hammedashiru89@gmail.com>
2025-05-02 08:29:02 +0100

    feat: update AccountDropdown with additional menu items and styling adjustments

--- components/custom/account-dropdown.tsx
+++ components/custom/account-dropdown.tsx
@@ -15,16 +15,28 @@ export function AccountDropdown() {
         <UserIcon className='cursor-pointer' />
       </DropdownMenuTrigger>
 
-      <DropdownMenuContent className='w-64 mt-3 relative z-50' align='center'>
-        <DropdownMenuLabel className='text-center text-base font-semibold'>
+      <DropdownMenuContent
+        className='min-w-64 mt-3 relative z-50'
+        align='center'>
+        <DropdownMenuLabel className='text-center md:text-xl text-base font-semibold pt-3'>
           My Account
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>My Profile</DropdownMenuItem>
-        <DropdownMenuItem>Orders</DropdownMenuItem>
-        <DropdownMenuItem>My Received Gift Cards</DropdownMenuItem>
+        <DropdownMenuItem className='font-dm-sans py-3 px-10 text-base'>
+          My Profile
+        </DropdownMenuItem>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className='text-red-500'>Sign Out</DropdownMenuItem>
+        <DropdownMenuItem className='font-dm-sans py-3 px-10 text-base'>
+          Orders
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className='font-dm-sans py-3 px-10 text-base'>
+          My Received Gift Cards
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className='text-red-500 font-dm-sans py-3 px-10 text-base'>
+          Sign Out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
