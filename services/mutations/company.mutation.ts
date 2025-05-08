7bd7375c1d7757ce421439b2cb72600c90b21651
ashiruhammed <hammedashiru89@gmail.com>
2025-05-09 00:39:46 +0100

    feat: enhance useUpdateCompanyDetails hook with company dashboard data integration

--- services/mutations/company.mutation.ts
+++ services/mutations/company.mutation.ts
@@ -1,15 +1,26 @@
 import { companyDetailsSchema } from '@/libs/schema';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useQueryClient } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { company_keys } from '../queries/company.queries';
+import { AxiosResponse } from 'axios';
+import { ApiCompanyDetailsResponse } from '@/libs/types/brand.types';
 
 export const useUpdateCompanyDetails = () => {
+  const queryClient = useQueryClient();
+  const company_dashboard:
+    | AxiosResponse<ApiCompanyDetailsResponse>
+    | undefined = queryClient.getQueryData(company_keys.company_dashboard());
+
+  console.log('Company dashboard data:', company_dashboard?.data);
+
   const form = useForm<z.infer<typeof companyDetailsSchema>>({
     resolver: zodResolver(companyDetailsSchema),
     defaultValues: {
-      company_name: '',
-      company_email: '',
-      phone_number: '',
+      company_name: company_dashboard?.data?.company_name || '',
+      company_email: company_dashboard?.data?.company_address || '',
+      phone_number: company_dashboard?.data?.registration_number || '',
       password: '',
     },
   });
