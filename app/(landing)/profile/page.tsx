b8f5ec49729f51ea60f8b675774e34ca3367c59f
ashiruhammed <hammedashiru89@gmail.com>
2025-05-12 13:07:08 +0100

    feat: implement ProfilePage component and add ProfileIcon

--- app/(landing)/profile/page.tsx
+++ app/(landing)/profile/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import ProfileIcon from '@/components/icon/profile-icon';
 
-function ProilePage() {
-  return <div>ProilePage</div>;
+function ProfilePage() {
+  return (
+    <div className='container mx-auto px-4 md:pt-[75px] pt-10'>
+      <div className='flex items-center gap-4 pb-4 border-b md:pb-6'>
+        <ProfileIcon />
+        <h1 className='font-bold md:text-base text-sm'>Attah Inyang</h1>
+      </div>
+    </div>
+  );
 }
 
-export default ProilePage;
+export default ProfilePage;
