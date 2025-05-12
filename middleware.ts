d7bee7283ae390ff9cc43a63cb165dd5bcbe67d5
ashiruhammed <hammedashiru89@gmail.com>
2025-05-12 13:45:08 +0100

    fix: improve user type handling in middleware for admin route access

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,10 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
   const accessToken = req.cookies.get('access_token');
-  const userType = req.cookies.get('user_type') as any;
+  const userType = req.cookies.get('user_type');
 
   const url = req.nextUrl.clone();
+  console.log('userType', userType);
 
   if (!accessToken && url.pathname !== '/auth/sign-in') {
     return NextResponse.redirect(new URL('/auth/sign-in', req.url));
@@ -15,9 +16,8 @@ export function middleware(req: NextRequest) {
   if (accessToken && url.pathname === '/auth/sign-in') {
     return NextResponse.redirect(new URL('/', req.url)); // Adjust the redirect URL as needed
   }
-
   // Check if the user is an admin
-  if (userType !== 'MERCHANT' && url.pathname.startsWith('/admin')) {
+  if (userType?.value !== 'MERCHANT' && url.pathname.startsWith('/admin')) {
     return NextResponse.redirect(new URL('/', req.url)); // Redirect non-admin users away from admin routes
   }
 
