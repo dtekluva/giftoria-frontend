cdf0a6a244a3a1c96d5b9c986914b5333233f685
ashiruhammed <hammedashiru89@gmail.com>
2025-05-19 12:05:48 +0100

    feat: enhance authentication middleware to handle cashier sign-in and improve user type checks

--- middleware.ts
+++ middleware.ts
@@ -7,17 +7,28 @@ export function middleware(req: NextRequest) {
   const userType = req.cookies.get('user_type');
 
   const url = req.nextUrl.clone();
-  console.log('userType', userType);
 
-  if (!accessToken && url.pathname !== '/auth/sign-in') {
-    return NextResponse.redirect(new URL('/auth/sign-in', req.url));
+  // Check if the user is trying to access a protected route without authentication
+
+  if (!accessToken && !url.pathname.startsWith('/auth')) {
+    if (url.pathname.startsWith('/auth/cashier')) {
+      return NextResponse.redirect(new URL('/auth/cashier/sign-in', req.url)); // Redirect to cashier sign-in
+    } else {
+      return NextResponse.redirect(new URL('/auth/sign-in', req.url)); // Redirect to sign-in
+    }
   }
 
   if (accessToken && url.pathname === '/auth/sign-in') {
     return NextResponse.redirect(new URL('/', req.url)); // Adjust the redirect URL as needed
   }
+  if (accessToken && url.pathname === '/auth/cashier/sign-in') {
+    return NextResponse.redirect(new URL('/', req.url)); // Adjust the redirect URL as needed
+  }
   // Check if the user is an admin
   if (userType?.value !== 'MERCHANT' && url.pathname.startsWith('/admin')) {
+    return NextResponse.redirect(new URL('/', req.url)); // Redirect non-admin users away from admin routes
+  }
+  if (userType?.value !== 'CASHIER' && url.pathname.startsWith('/cashier')) {
     return NextResponse.redirect(new URL('/', req.url)); // Redirect non-admin users away from admin routes
   }
 
@@ -28,6 +39,9 @@ export function middleware(req: NextRequest) {
 export const config = {
   matcher: [
     '/auth/sign-in',
+    '/auth/cashier/sign-in',
+    '/auth/cashier/:path*',
+    '/cashier/:path*',
     '/my-orders',
     '/order-details/:path*',
     '/order-summary',
