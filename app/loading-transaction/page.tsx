ccc7084f66ab398b8d82ab9224bca9fc15c81e76
ashiruhammed <hammedashiru89@gmail.com>
2025-05-22 18:52:02 +0100

    feat: enhance LoadingTransactions component with navigation link and improved layout

--- app/loading-transaction/page.tsx
+++ app/loading-transaction/page.tsx
@@ -1,11 +1,19 @@
+import HomeIcon from '@/components/icon/home-icon';
 import { TextShimmer } from '@/components/ui/text-shimmer';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 function LoadingTransactions() {
   return (
-    <div className='flex items-center justify-center h-screen w-full'>
-      <div className='flex justifyc-center flex-col items-center'>
+    <div className='flex flex-col h-screen w-full'>
+      <Link
+        href={'/'}
+        className='bg-[#4A014A] flex rounded-full h-fit my-4 ml-auto mr-8 py-2 px-6 gap-2'>
+        <HomeIcon />
+        <p className='text-[#D9D9D9] font-semibold font-nunito'>Home</p>
+      </Link>
+      <div className='flex self-center max-w-fit mx-auto justify-center flex-col items-center'>
         <Image
           src={'/assets/loading-transaction.png'}
           alt='Loading'
