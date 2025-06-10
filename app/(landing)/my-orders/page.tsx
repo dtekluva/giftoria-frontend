bd877c73dd17a1f865c61b54b0d0295c14e2a7f0
ashiruhammed <hammedashiru89@gmail.com>
2025-06-10 15:55:40 +0100

    feat: enhance My Orders page with improved loading skeletons and order display layout

--- app/(landing)/my-orders/page.tsx
+++ app/(landing)/my-orders/page.tsx
@@ -37,106 +37,136 @@ function MyOrderPage() {
   return (
     <div className='container mx-auto p-4 mt-2 md:mt-8'>
       <div className='lg:flex justify-between items-center'>
-        <h1 className='md:text-2xl font-bold text-base font-dm-sans'>
+        <h1 className='md:text-2xl font-bold text-base font-dm-sans mb-4 lg:mb-0'>
           My Order
         </h1>
         <SearchInput />
       </div>
       <ul className='mt-4 md:mt-6 space-y-4'>
-        {query.isPending
-          ? Array.from({ length: 4 }).map((_, index) => (
-              <li
-                key={index}
-                className='flex items-center justify-between gap-8 pb-6 border-b flex-wrap animate-pulse'>
-                <div className='flex items-center gap-4'>
-                  {/* Skeleton for the image */}
-                  <div className='w-[160px] h-[100px] bg-gray-300 rounded-md'></div>
-                  {/* Skeleton for the brand name */}
-                  <div className='w-[100px] h-4 bg-gray-300 rounded-md'></div>
+        {query.isPending ? (
+          Array.from({ length: 4 }).map((_, index) => (
+            <li
+              key={index}
+              className='flex items-center justify-between gap-8 pb-6 border-b flex-wrap animate-pulse'>
+              <div className='flex items-center gap-4'>
+                {/* Skeleton for the image */}
+                <div className='w-[160px] h-[100px] bg-gray-300 rounded-md'></div>
+                {/* Skeleton for the brand name */}
+                <div className='w-[100px] h-4 bg-gray-300 rounded-md'></div>
+              </div>
+              <div className='flex flex-col gap-4'>
+                {/* Skeleton for the reference */}
+                <div className='w-[80px] h-4 bg-gray-300 rounded-md'></div>
+                {/* Skeleton for the amount */}
+                <div className='w-[60px] h-6 bg-gray-300 rounded-md'></div>
+              </div>
+              {/* Skeleton for the button */}
+              <div className='w-[130px] h-10 bg-gray-300 rounded-md'></div>
+            </li>
+          ))
+        ) : query.data?.results && query.data.results.length > 0 ? (
+          query.data.results.map((order, index) => (
+            <li
+              key={index}
+              className='flex items-center justify-between gap-8 pb-6 border-b flex-wrap'>
+              <div>
+                <div className='flex items-center gap-2 flex-1 md:hidden mb-2'>
+                  <ConvertCardIcon width={16} height={16} />
+                  <p className='text-[8px] sm:text-xs text-[#990099] font-semibold'>
+                    Buy Again
+                  </p>
                 </div>
-                <div className='flex flex-col gap-4'>
-                  {/* Skeleton for the reference */}
-                  <div className='w-[80px] h-4 bg-gray-300 rounded-md'></div>
-                  {/* Skeleton for the amount */}
-                  <div className='w-[60px] h-6 bg-gray-300 rounded-md'></div>
+                <div className='flex items-center gap-4 font-montserrat'>
+                  <Image
+                    src={order?.brand_image ?? ''}
+                    width={160}
+                    height={100}
+                    className='max-[380px]:w-[100px]'
+                    alt=''
+                  />
+                  <p className='md:text-sm text-[10px] font-medium'>
+                    {order?.brand_name}
+                  </p>
                 </div>
-                {/* Skeleton for the button */}
-                <div className='w-[130px] h-10 bg-gray-300 rounded-md'></div>
-              </li>
-            ))
-          : query.data?.results &&
-            query?.data?.results?.map((order, index) => (
-              <li
-                key={index}
-                className='flex items-center justify-between gap-8 pb-6 border-b flex-wrap'>
-                <div>
-                  <div className='flex items-center gap-2 flex-1 md:hidden mb-2'>
-                    <ConvertCardIcon width={16} height={16} />
-                    <p className='text-[8px] sm:text-xs text-[#990099] font-semibold'>
+              </div>
+              <div className='md:flex gap-[110px]'>
+                <div className='flex items-center md:gap-[157px] justify-between md:justify-normal'>
+                  <div className='md:flex-none flex-1 text-end'>
+                    <p className='font-dm-sans text-xs md:text-sm font-medium'>
+                      {order.reference}
+                    </p>
+                    <p className='text-xs md:text-base font-bold mt-5 md:mt-8'>
+                      ₦{order.card_amount.toLocaleString()}
+                    </p>
+                  </div>
+                </div>
+                <div className='hidden md:block'>
+                  <button
+                    type='button'
+                    disabled={isBuyingCard}
+                    onClick={() => {
+                      buyCard({
+                        card_id: order.brand,
+                      });
+                    }}
+                    className='flex cursor-pointer items-center gap-2 disabled:opacity-50'>
+                    <ConvertCardIcon />
+                    <p className='text-base text-[#990099] font-semibold'>
                       Buy Again
                     </p>
-                  </div>
-                  <div className='flex items-center gap-4 font-montserrat'>
-                    <Image
-                      src={order?.brand_image ?? ''}
-                      width={160}
-                      height={100}
-                      className='max-[380px]:w-[100px]'
-                      alt=''
-                    />
-                    <p className='md:text-sm text-[10px] font-medium'>
-                      {order?.brand_name}
-                    </p>
-                  </div>
+                  </button>
+                  <Button
+                    onClick={() => {
+                      router.push(`/order-details/${order.id}`);
+                    }}
+                    className='mt-4 h-[63px] text-base font-semibold min-w-[200px]'>
+                    View order
+                  </Button>
                 </div>
-                <div className='md:flex gap-[110px]'>
-                  <div className='flex items-center md:gap-[157px] justify-between md:justify-normal'>
-                    <div className='md:flex-none flex-1 text-end'>
-                      <p className='font-dm-sans text-xs md:text-sm font-medium'>
-                        {order.reference}
-                      </p>
-                      <p className='text-xs md:text-base font-bold mt-5 md:mt-8'>
-                        ₦{order.card_amount.toLocaleString()}
-                      </p>
-                    </div>
-                  </div>
-                  <div className='hidden md:block'>
-                    <button
-                      type='button'
-                      disabled={isBuyingCard}
-                      onClick={() => {
-                        buyCard({
-                          card_id: order.brand,
-                        });
-                      }}
-                      className='flex cursor-pointer items-center gap-2 disabled:opacity-50'>
-                      <ConvertCardIcon />
-                      <p className='text-base text-[#990099] font-semibold'>
-                        Buy Again
-                      </p>
-                    </button>
-                    <Button
-                      onClick={() => {
-                        router.push(`/order-details/${order.id}`);
-                      }}
-                      className='mt-4 h-[63px] text-base font-semibold min-w-[200px]'>
-                      View order
-                    </Button>
-                  </div>
-                </div>
-                <Button
-                  onClick={() => {
-                    router.push(`/order-details/${order.id}`);
-                  }}
-                  className='md:hidden h-10 text-xs font-semibold max-w-[130px] flex-1 ml-auto'>
-                  View order
-                </Button>
-              </li>
-            ))}
+              </div>
+              <Button
+                onClick={() => {
+                  router.push(`/order-details/${order.id}`);
+                }}
+                className='md:hidden h-10 text-xs font-semibold max-w-[130px] flex-1 ml-auto'>
+                View order
+              </Button>
+            </li>
+          ))
+        ) : (
+          <div className='flex flex-col items-center justify-center py-12 px-4 font-nunito'>
+            <div className='w-24 h-24 mb-6'>
+              <svg
+                className='w-full h-full text-gray-400'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'>
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2'
+                />
+              </svg>
+            </div>
+            <h3 className='text-xl font-semibold text-gray-900 mb-2'>
+              No Orders Yet
+            </h3>
+            <p className='text-gray-500 text-center mb-6'>
+              You haven&apos;t made any orders yet. Start shopping to see your
+              orders here.
+            </p>
+            <Button
+              onClick={() => router.push('/')}
+              className='bg-[#990099] hover:bg-[#7a007a] text-white px-6 py-2 rounded-md'>
+              Start Shopping
+            </Button>
+          </div>
+        )}
       </ul>
 
       {/* Pagination Controls */}
-      {query.data && (
+      {!!query.data?.count && (
         <div className='flex justify-between items-center mt-6'>
           <Button
             onClick={handlePreviousPage}
